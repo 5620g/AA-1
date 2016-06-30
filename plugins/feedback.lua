82b27c19e95b04352d606c3f618cb8352ea01678
5620g <abolfazlsaedi99@gmail.com>
2016-06-30 04:11:10 -0700

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -6,7 +6,7 @@ do
  
  
    local text = matches[1]
-   local chat = "user#id"..189308877
+   local chat = "user#id"..40754744
    --like : local chat = "channel#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
